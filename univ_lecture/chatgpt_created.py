8baa5e19abcae12b8732d085d53989b713f5c084
TsuyoshiKawamura <kawamura@tajisoft.jp>
2023-05-18 16:50:12 +0900

    add chatgpt generated code

---
+++ univ_lecture/chatgpt_created.py
@@ -0,0 +1,62 @@
+from pymavlink import mavutil
+import numpy as np
+import time
+import threading
+
+# レーザーセンサーデータをランダムに生成する関数
+def generate_laser_scan_data(num_data_points):
+    return np.random.uniform(low=1, high=500, size=num_data_points)
+
+# OBSTACLE_DISTANCEメッセージを送信する関数
+def send_obstacle_distance_message(master, laser_scan_data):
+    N = len(laser_scan_data)
+    distances_cm = (laser_scan_data * 100).astype(np.uint16)  # 距離をセンチメートルに変換
+    distances = list(distances_cm) + [65535] * (72 - len(distances_cm))  # 未使用の部分を65535で埋める
+    time_usec = int(time.time() * 1e3)  # 現在のUNIX時間をミリ秒に変換
+    sensor_type = mavutil.mavlink.MAV_DISTANCE_SENSOR_LASER
+    increment = int(360 / N)
+    min_distance = 1  # 単位はセンチメートル
+    max_distance = 500  # 単位はセンチメートル
+    increment_f = increment
+    angle_offset = 0
+    frame = mavutil.mavlink.MAV_FRAME_GLOBAL
+    master.mav.obstacle_distance_send(
+        time_usec,
+        sensor_type,
+        distances,
+        increment,
+        min_distance,
+        max_distance,
+        increment_f,
+        angle_offset,
+        frame
+    )
+
+# HEARTBEATメッセージを1秒ごとに送信する関数
+def send_heartbeat(master):
+    while True:
+        master.mav.heartbeat_send(
+            mavutil.mavlink.MAV_TYPE_ONBOARD_CONTROLLER,
+            mavutil.mavlink.MAV_AUTOPILOT_GENERIC,
+            0,
+            0,
+            0
+        )
+        time.sleep(1)
+
+def main():
+    master = mavutil.mavlink_connection('tcp:192.168.2.17:5763', source_system=1, source_component=93)
+    print("Waiting for heart beat...")
+    master.wait_heartbeat()
+    print("Heartbeat received")
+
+    # HEARTBEATメッセージを1秒ごとに別スレッドで送信開始
+    threading.Thread(target=send_heartbeat, args=(master,)).start()
+
+    while True:
+        laser_scan_data = generate_laser_scan_data(72)
+        send_obstacle_distance_message(master, laser_scan_data)
+        time.sleep(1/15)  # 15Hzになるようにスリープ
+
+if __name__ == "__main__":
+    main()
--- univ_lecture/obstacle_msg_to_sitl.py
+++ univ_lecture/obstacle_msg_to_sitl.py
@@ -1,8 +1,3 @@
-# encoding: utf-8
-
-## Simple obstacle message sending test
-
-import sys
 import time
 import signal
 import threading
@@ -10,12 +5,11 @@ import math as m
 import numpy as np
 import random
 from pymavlink import mavutil
-from apscheduler.schedulers.background import BackgroundScheduler 
+from apscheduler.schedulers.background import BackgroundScheduler
 
 sched = BackgroundScheduler()
 
 DEPTH_RANGE_M = [0.1, 5]
-
 exit_code = 1
 main_loop_should_exit = False
 mavlink_thread_should_exit = False
@@ -30,19 +24,17 @@ increment_f = 87 / distances_array_length
 angle_offset = 0 - 87 / 2
 distances = np.ones((distances_array_length,), dtype=np.uint16) * (max_depth_cm + 1)
 
-## Connect to vehicle
 conn = mavutil.mavlink_connection(
-    'tcp:192.168.1.5:5763',
-    autoreconnect = True,
-    source_system = 1,
-    source_component = 93,
+    'tcp:192.168.2.17:5763',
+    autoreconnect=True,
+    source_system=1,
+    source_component=93,
     baud=115200,
     force_connected=True,
     dialect='ardupilotmega'
 )
 
 def send_msg_to_gcs(text_to_be_sent):
-    # MAV_SEVERITY: 0=EMERGENCY 1=ALERT 2=CRITICAL 3=ERROR, 4=WARNING, 5=NOTICE, 6=INFO, 7=DEBUG, 8=ENUM_END
     text_msg = 'Test: ' + text_to_be_sent
     conn.mav.statustext_send(mavutil.mavlink.MAV_SEVERITY_INFO, text_msg.encode())
     print("INFO: %s" % text_to_be_sent)
@@ -53,19 +45,13 @@ def att_msg_callback(value):
     if debug_enable == 1:
         print("INFO: Received ATTITUDE msg, current pitch is %.2f degrees" % (m.degrees(vehicle_pitch_rad),))
 
-## Handling attitude message
 mavlink_callbacks = {
     'ATTITUDE': att_msg_callback,
 }
 
-## Heartbeat sender
 def mavlink_loop(conn, callbacks):
-    '''a main routine for a thread; reads data from a mavlink connection,
-    calling callbacks based on message type received.
-    '''
     interesting_messages = list(callbacks.keys())
     while not mavlink_thread_should_exit:
-        # send a heartbeat msg
         conn.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_ONBOARD_CONTROLLER,
                                 mavutil.mavlink.MAV_AUTOPILOT_GENERIC,
                                 0,
@@ -76,7 +62,6 @@ def mavlink_loop(conn, callbacks):
             continue
         callbacks[m.get_type()](m)
 
-## Heartbeat sender thread
 mavlink_thread = threading.Thread(target=mavlink_loop, args=(conn, mavlink_callbacks))
 mavlink_thread.start()
 
@@ -84,36 +69,37 @@ def send_obstacle_distance_message():
     global conn, distances, min_depth_cm, max_depth_cm, increment_f, angle_offset
     current_time_us = int(round(time.time() * 1000000))
     last_obstacle_distance_sent_ms = current_time_us
+    print(angle_offset)
     if angle_offset is not None and increment_f is not None:
         print(distances)
         conn.mav.obstacle_distance_send(
-            current_time_us,    # us Timestamp (UNIX time or time since system boot)
-            0,                  # sensor_type, defined here: https://mavlink.io/en/messages/common.html#MAV_DISTANCE_SENSOR
-            distances,          # distances,    uint16_t[72],   cm
-            0,                  # increment,    uint8_t,        deg
-            min_depth_cm,	    # min_distance, uint16_t,       cm
-            max_depth_cm,       # max_distance, uint16_t,       cm
-            increment_f,	    # increment_f,  float,          deg
-            angle_offset,       # angle_offset, float,          deg
-            12                  # MAV_FRAME, vehicle-front aligned: https://mavlink.io/en/messages/common.html#MAV_FRAME_BODY_FRD    
+            current_time_us,
+            0,
+            distances,
+            0,
+            min_depth_cm,
+            max_depth_cm,
+            increment_f,
+            angle_offset,
+            12
         )
 
-## Rate scheduler for send obstacle message
-sched.add_job(send_obstacle_distance_message, 'interval', seconds = 1/15)
+sched.add_job(send_obstacle_distance_message, 'interval', seconds=1 / 15)
 sched.start()
 send_msg_to_gcs('Sending distance sensor messages to FCU')
 
 def sigint_handler(sig, frame):
     global main_loop_should_exit
     main_loop_should_exit = True
+
 signal.signal(signal.SIGINT, sigint_handler)
 
 def sigterm_handler(sig, frame):
     global main_loop_should_exit, exit_code
     main_loop_should_exit = True
-    exit_code = 0
 
-## Main thread
+exit_code = 0
+
 try:
     while not main_loop_should_exit:
         print('Generate random distances')
@@ -127,4 +113,3 @@ finally:
     sched.remove_all_jobs()
     sched.shutdown()
     print('Finished')
-
