f3483105539f29ecacc6d3c5d6a4c3f253a62a89
kuramoto.yoshikazu@gmail.com <kuramoto.yoshikazu@gmail.com>
2024-06-19 14:25:16 +0100

    correct pm01_message_dump.py

--- pymavlink_scripts/pm01_message_dump.py
+++ pymavlink_scripts/pm01_message_dump.py
@@ -3,7 +3,7 @@ import time
 
 # 機体への接続
 master: mavutil.mavfile = mavutil.mavlink_connection(
-    "127.0.0.1:14551", source_system=1, source_component=90)
+    "tcp:127.0.0.1:5762", source_system=1, source_component=90)
 master.wait_heartbeat()
 
 # 全メッセージを10Hzで受信
