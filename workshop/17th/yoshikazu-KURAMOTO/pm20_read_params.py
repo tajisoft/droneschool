66e1d6cc3f4e50a22c20664829457d3be41e902a
kuramoto.yoshikazu@gmail.com <kuramoto.yoshikazu@gmail.com>
2024-06-27 15:51:52 +0100

    20240627_2

--- workshop/17th/yoshikazu-KURAMOTO/pm20_read_params.py
+++ workshop/17th/yoshikazu-KURAMOTO/pm20_read_params.py
@@ -10,7 +10,7 @@ master : mavutil.mavfile = mavutil.mavlink_connection(
  )
 
  # Wait a heartbeat before sending commands(HEARTBEATを待つ)
- master.wait_heartbeat()
+master.wait_heartbeat()
 
 # Request all parameters
 master.mav.param_request_list_send(
@@ -21,5 +21,10 @@ while True:
     time.sleep(0.01)
     try:
         message = master.recv_match(
-            type='PARAM_VALUE'
-        )
+            type='PARAM_VALUE',blocking=True
+        ).to_dict()
+        print('name: %s\tvalue: %d' % (message['param_id'],message['param_value']))
+        
+    except Exception as error:
+        print(error)
+        sys.exit(0)
