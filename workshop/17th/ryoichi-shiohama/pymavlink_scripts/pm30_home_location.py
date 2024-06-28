baf9b175336ec261e2cf296fc71c9464fe325657
Ryoichi <ryoichi.shiohama@gmail.com>
2024-06-28 18:03:06 +0100

    デバッグ用にメッセージ出力のアレンジを加えた

--- workshop/17th/ryoichi-shiohama/pymavlink_scripts/pm30_home_location.py
+++ workshop/17th/ryoichi-shiohama/pymavlink_scripts/pm30_home_location.py
@@ -6,27 +6,36 @@ master: mavutil.mavfile = mavutil.mavlink_connection(
   source_system=1,
   source_component=90
 )
+print("接続を試みています...")
 master.wait_heartbeat()
+print("ハートビートを受信しました。接続に成功しました。")
 
 # メッセージレート変更
-# msg = master.mav.request_data_streatm_encode(
+# msg = master.mav.request_data_stream_encode(
 #   master.target_system,
 #   master.target_component,
 #   0, 10, 1
 # )
 # master.mav.send(msg)
 
-while True:
-  # HOME_POSITIONメッセージを受信、アームのタイミングで受信できる
-  msg = master.recv_match(type='HOME_POSITION', blocking=True)
-  if msg is not None:
-    # ホームロケーション情報を設定
-    home_latitude = msg.latitude / 1.0e7
-    home_longitude = msg.longitude / 1.0e7
-    home_altitude = msg.altitude / 1000.0 # メートル単位に変換
-    
-    print(
-      f"ホームロケーション: lat={home_latitude}, long={home_longitude}, alt={home_altitude}"
-    )
-    break # ホームロケーションを取得したら終了
+try:
+  while True:
+    print("メッセージを待っています...")
+    # HOME_POSITIONメッセージを受信、アームのタイミングで受信できる
+    msg = master.recv_match(type='HOME_POSITION', blocking=False)
+    print("メッセージの取得を試みました。")
+    if msg is not None:
+      # ホームロケーション情報を設定
+      home_latitude = msg.latitude / 1.0e7
+      home_longitude = msg.longitude / 1.0e7
+      home_altitude = msg.altitude / 1000.0 # メートル単位に変換
+      
+      print(
+        f"ホームロケーション: lat={home_latitude}, long={home_longitude}, alt={home_altitude}"
+      )
+      break # ホームロケーションを取得したら終了
+    else:
+      print("メッセージがありません。")
+except Exception as e:
+  print(f"エラーが発生しました: {e}")
 
