76821d56222c584d9f6132d9b5b253a4b5684ddb
Ryoichi <ryoichi.shiohama@gmail.com>
2024-07-02 16:20:53 +0100

    テイクオフの処理追加

--- workshop/17th/ryoichi-shiohama/pymavlink_scripts/day3_homework_original_app.py
+++ workshop/17th/ryoichi-shiohama/pymavlink_scripts/day3_homework_original_app.py
@@ -20,3 +20,44 @@ while True:
     break
   master.recv_msg()
 print("モード変更完了")
+
+# アーム
+master.arducopter_arm()
+master.motors_armed_wait()
+print("アーム完了")
+
+# 目標高度
+target_altitude = 3
+
+# 離陸
+master.mav.command_long_send(
+  master.target_system,
+  master.target_component,
+  mavutil.mavlink.MAV_CMD_MAV_TAKEOFF,
+  0, 0, 0, 0, 0, 0, 0,
+  target_altitude
+)
+
+# TODO: メッセージレート変更か？検討する
+
+# 目標高度への到達を確認
+while True:
+  # GLOBAL_POSITION_INT から相対高度を取得
+  recieved_msg = master.recv_match(
+    type='GLOBAL_POSITION_INT',
+    blocking=True
+  )
+  current_altitude = recieved_msg.relative_alt / 1000
+
+  print(f"高度:{current_altitude}")
+
+  if current_altitude >= target_altitude * 0.95:
+    print("目標高度に到達")
+    break
+
+  time.sleep(0.1)
+
+# TODO:目標高度到達後、任意の軌道を描く（まずは三角など、次に努力目標で星？）
+
+# 切断
+master.close()
