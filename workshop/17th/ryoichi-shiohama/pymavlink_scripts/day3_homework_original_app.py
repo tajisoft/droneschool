b3e75d49b5a7aafed1993723dd1fdf8d6276b750
Ryoichi <ryoichi.shiohama@gmail.com>
2024-07-02 09:03:10 +0100

    接続コード追加

--- workshop/17th/ryoichi-shiohama/pymavlink_scripts/day3_homework_original_app.py
+++ workshop/17th/ryoichi-shiohama/pymavlink_scripts/day3_homework_original_app.py
@@ -1,1 +1,11 @@
-# TODO: 宿題のオリジナルアプリケーションを作成する
+from pymavlink import mavutil
+import time
+
+# 機体（シミュレータ）への接続
+master: mavutil.mavfile = mavutil.mavlink_connection(
+  "tcp:10.0.2.135:5762",
+  source_system=1,
+  source_component=90
+)
+master.wait_heartbeat()
+print("接続完了")
