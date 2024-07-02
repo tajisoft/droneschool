89e76573644dd95bb8d47f256937fc59c016fcd5
kaw803 <kawasaki.hiroaki@gmail.com>
2024-07-02 22:11:27 +0900

    use array

--- workshop/17th/hiroaki-kawasaki/_motor_check.lua
+++ workshop/17th/hiroaki-kawasaki/_motor_check.lua
@@ -2,14 +2,15 @@
 
 ---@diagnostic disable: need-check-nil
 
-local port = serial:find_serial(0)
 local MODE_ACRO = 1
 local OUTPUT_PWM_DEFAULT = 1000 * 0.07 + 1000
 local EXP_RPM = 1500
 local OK_RANGE = 0.2
 
+local port = serial:find_serial(0)
 local motor_array = {0, 3, 1, 2}
 local rx_data = {}
+local rpm = {}
 
 local tries = 0
 local motor_index = -1
@@ -43,35 +44,26 @@ function get_rpm()
     if port:available() >= 10 then  -- 必要なバイト数があるか確認
 
       -- 各バイトを整数に変換
-      local byte0 = port:read()
+      rx_data[1] = port:read()
 
       -- 最初のバイトが0xffであることを確認
-      if byte0 == 0xff then
-        local byte1 = port:read()
-        local byte2 = port:read()
-        local byte3 = port:read()
-        local byte4 = port:read()
-        local byte5 = port:read()
-        local byte6 = port:read()
-        local byte7 = port:read()
-        local byte8 = port:read()
-        local byte9 = port:read()
+      if rx_data[1] == 0xff then
+        local sum = 0
+        for i = 2, 9 do
+          rx_data[i] = port:read()
+          sum = (sum + rx_data[i]) & 0xff
+        end
 
-        local sum = byte1 + byte2 + byte3 + byte4 + byte5 + byte6 + byte7 + byte8
-        sum = sum & 0xff 
-
-        -- 排他的論理和をチェック
-        if byte9 == sum then
-          -- 正しいデータを取得
-          -- 16ビット整数データを組み立てる
-          rpm1 = byte1 + (byte2 * 256)  -- LOW8bitとHIGH8bitから16ビット整数を構築
-          rpm2 = byte3 + (byte4 * 256)
-          rpm3 = byte5 + (byte6 * 256)
-          rpm4 = byte7 + (byte8 * 256)
+        rx_data[10] = port:read()
+        if rx_data[10] == sum then
+          -- 正しいデータを取得できたら、16ビット整数データを組み立てる
+          for i = 1, 4 do
+            rpm[i] = rx_data[i * 2] + (rx_data[i * 2 + 1] * 256)
+          end
           return 0
         else
           -- データエラー時の処理
-          gcs:send_text(0, "Data error: SUM mismatch")
+          gcs:send_text(0, "Data error: SUM mismatch  exp=" .. rx_data[10] .. "  sum=" .. sum)
         end
       else
         -- フォーマットエラー時、読み捨て
@@ -114,20 +106,9 @@ function task ()
     end
   end
 
-  local rpm = get_rpm()
-  if rpm >= 0 and motor_index > 0 then      
-    gcs:send_text(0, "motor_index rpm: " .. rpm1 .." ".. rpm2 .." " .. rpm3 .." " .. rpm4)
-    if motor_array[motor_index] == 0 then
-      rpm=rpm2
-    elseif motor_array[motor_index] == 1 then
-      rpm=rpm3
-    elseif motor_array[motor_index] == 2 then
-      rpm=rpm1
-    else
-      rpm=rpm4
-      
-    end
-    if rpm > EXP_RPM * (1 - OK_RANGE) and rpm < EXP_RPM * (1 + OK_RANGE) then
+  if get_rpm() >= 0 and motor_index > 0 then      
+    gcs:send_text(0, "rpm: " .. rpm[1] ..", ".. rpm[2] ..", " .. rpm[3] ..", " .. rpm[4])
+    if rpm[motor_array[motor_index] + 1] > EXP_RPM * (1 - OK_RANGE) and rpm[motor_array[motor_index] + 1] < EXP_RPM * (1 + OK_RANGE) then
       ok_count = ok_count + 1
       if ok_count >= 3 then 
         gcs:send_text(0, ">>> MTR" .. motor_index .. ": OK")
