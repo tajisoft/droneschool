c0270217d1c08e67ec634c0735540104b42acf9e
Yuji Sakamoto <yuji@sk.rdy.jp>
2024-12-02 20:48:11 +0900

    ドローンエンジニア養成塾18期 コース３：モード追加実習（坂本案１） simpleモード切替制御 ※init()とexit()の実行タイミングで処理の順番が変わるように見えるのでタイミング調整  - mode_althold_simple  - mode_loiter_super_simple

---
+++ workshop/18th/yuji-sakamoto/fc/Lua/force-change-simple.lua
@@ -0,0 +1,41 @@
+-- RC8スイッチがONの時現在のモードを監視してALT_HOLDの時はALT_HOLD_SIMPLE(100)に変更する
+-- 同様にLOITERの時はLOITER_SUPERSIMPLE(101)に変更する
+--
+
+local MODE_ALT_HOLD = 2
+local MODE_LOITER = 5
+local MODE_ALT_HOLD_SIMPLE = 100
+local MODE_LOITER_SUPERSIMPLE = 101
+local prior_mode = -1
+local prior_mode_name = "NONE"
+
+-- the main update function
+function update()
+    pwm8 = rc:get_pwm(8)
+    if pwm8 and pwm8 > 1500 then
+      -- RC 8 がONの時のみ有効化
+      local mode = vehicle:get_mode()                   -- get current mode
+      if mode == MODE_ALT_HOLD then
+        -- ALT_HOLDならALT_HOLD_SIMPLEにモード変更する
+        gcs:send_text(MAV_SEVERITY.INFO, "Lua : ALT_HOLD -> ALT_HOLD_SIMPLE")
+        prior_mode = mode
+        prior_mode_name = "ALT_HOLD"
+        vehicle:set_mode(MODE_ALT_HOLD_SIMPLE)
+      else if mode == MODE_LOITER then
+        -- LOITERならLOITER_SUPERSIMPLEにモード変更する
+        gcs:send_text(MAV_SEVERITY.INFO, "Lua : LOITER -> LOITER_SUPERSIMPLE")
+        prior_mode = mode
+        prior_mode_name = "LOITER"
+        vehicle:set_mode(MODE_LOITER_SUPERSIMPLE)
+      end
+    else if prior_mode > 0 then
+      -- RC 8 がOFFの時切り替え済みの場合は切り替え前のモードに戻す
+      gcs:send_text(MAV_SEVERITY.INFO, "Lua : RESET to %s",prior_mode_name)
+      vehicle:set_mode(prior_mode)
+      prior_mode = -1
+    end
+
+  return update, 1000
+end
+
+return update()
