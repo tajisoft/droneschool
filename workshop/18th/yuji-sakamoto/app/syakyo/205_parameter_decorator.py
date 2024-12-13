e1b90fe8cb4ac3f8546611cdf9f705eb7ee25535
Yuji Sakamoto @ rpanion <yuji@sk.rdy.jp>
2024-12-13 12:35:01 +0900

    ドローンエンジニア養成塾18期
    コース２写経サンプルスクリプト修正
    
    dronekit版
    
    実機接続した上で動作確認OK

--- workshop/18th/yuji-sakamoto/app/syakyo/205_parameter_decorator.py
+++ workshop/18th/yuji-sakamoto/app/syakyo/205_parameter_decorator.py
@@ -1,7 +1,6 @@
 # 実機接続に変更
-# ※実機接続でT10JからSTABILIZE->ALT_HOLD->LOITER->RTL->LOITER変更で確認
-# 元々記載されているRTL_ALTって何？->何を設定すればいいのかわからない
-# 以下の説明見てもよくわからない
+# 元々記載されているRTL_ALTって何？->パラメータ設定名
+# ・・・なのでパラメータの値を変更した時の通知処理
 # https://dronekit.netlify.app/guide/vehicle_state_and_parameters.html
 import time
 from dronekit import Vehicle, connect
@@ -10,11 +9,10 @@ from dronekit import Vehicle, connect
 # vehicle = connect('tcp:127.0.0.1:5762', wait_ready=True, timeout=60)
 vehicle = connect('127.0.0.1:14552', wait_ready=True, timeout=60)
 
-#RTL_ALTが変更された際に通知を受け取る
+# RTL_ALTが変更された際に通知を受け取る
+# MPなど別の接続からパラメータを変更して確認する
 #デコレーターの設定
-#@vehicle.parameters.on_attribute('RTL_ALT') # 確認できず
-#@vehicle.parameters.on_attribute('RTL_LOITER') # 確認できず
-@vehicle.parameters.on_attribute('LOITER') # 確認できず
+@vehicle.parameters.on_attribute('RTL_ALT') 
 def decorated_RTL_ALT_callback(self, attr_name, value):
 	print("デコレーター: %s が %s に変更されました！" % (attr_name, value))
 
